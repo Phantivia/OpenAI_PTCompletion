46a6ed15b2b7ffcacdbcbe850eed3e09bb40c484
Phantivia <phantivia@gmali.com>
2023-05-14 21:04:30 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os.path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,14 +7,14 @@ with open(os.path.join(HERE, "README.md")) as fid:
     README = fid.read()
 
 setup(
-    name='PTCompletion',
+    name='ptcompletion',
     version='0.1',
     description='Parallel Tasks Completion for OpenAI API, and more.',
     long_description=README,
     long_description_content_type="text/markdown",
     author='Phantivia',
     author_email='phantivia@gmail.com',
-    packages=['openai'],
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
